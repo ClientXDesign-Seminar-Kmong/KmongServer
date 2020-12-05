bb74018b4f9d2e01b486bef7ab7c8e8ac667478a
leehyunjong <ahajongs@gmail.com>
2020-12-06 01:53:26 +0900

    kmong server

---
+++ modules/util.js
@@ -0,0 +1,13 @@
+const { response } = require("express");
+
+module.exports={
+    success:(status,message,data)=>{
+        status,
+        message,
+        data
+    },
+    fail:(status,message)=>{
+        status,
+        message
+    }
+}
